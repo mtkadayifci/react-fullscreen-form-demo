a5ec50a4291e9d4bd84caa1c4706fe29b356ed24
Mustafa Turgay Kadayıfçı <mtkadayifci@gmail.com>
2018-09-24 13:01:16 +0300

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import 'normalize.css';
+import FForm from './containers/FForm/FForm';
+import Frame from './components/Frames/Frame/Frame';
+
+const App = () => (
+  <FForm title="Fullscreen Form React Demo">
+    <Frame title="What's your name?">
+      <input type="string" required />
+    </Frame>
+    <Frame title="What's your email address?" show={false}>
+      <input type="email" placeholder="mtkadayifci@gmail.com" required />
+    </Frame>
+    <Frame title="What's your phone number?">
+      <input type="phone" required />
+    </Frame>
+    <Frame title="How old are you?">
+      <input type="number" required />
+    </Frame>
+  </FForm>
+);
+
+export default App;
